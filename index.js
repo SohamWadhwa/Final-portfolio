83628f9a8fd4fc05dc46fc5237c4a0bb0f8fecf8
SohamWadhwa <soham.wadhwa0712@gmail.com>
2025-01-03 22:57:41 +0530

    ccc

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
@@ -16,4 +16,4 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-// module.exports = app;
+module.exports = app;
